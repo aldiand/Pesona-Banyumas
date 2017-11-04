d490b3f41b72122320536824980444dceffa476f
aldiand <supersandre@gmail.com>
2017-11-04 08:34:02 +0700

    Initial commit

---
+++ Assets/Scripts/ImageGallery.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageGallery : MonoBehaviour {
+    public Texture[] image;
+    int maxTextures;
+    int arrayPos = 0;
+    Renderer rend;
+	// Use this for initialization
+	void Start () {
+        maxTextures = image.Length-1;
+        rend = GetComponent<Renderer>();
+	}
+	
+    public void changeImage(){
+        rend.material.mainTexture = image[arrayPos];
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void next(){
+        if (arrayPos + 1 < maxTextures)
+        {
+            arrayPos++;
+        }
+        else
+        {
+            arrayPos = 0;
+        }
+        changeImage();
+    }
+    public void prev(){
+        if(arrayPos - 1 > 0)
+        {
+            arrayPos--;
+        }
+        else
+        {
+            arrayPos = maxTextures;
+        }
+        changeImage();
+    }
+
+}
---
+++ Assets/Scripts/PrevImageVirtualButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class PrevImageVirtualButton : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    private VirtualButtonBehaviour vButtonObject;
+    ImageGallery image;
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        image.prev();
+        Debug.Log("Button Pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        vButtonObject = GetComponent<VirtualButtonBehaviour>();
+        vButtonObject.RegisterEventHandler(this);
+        image = FindObjectOfType<ImageGallery>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
